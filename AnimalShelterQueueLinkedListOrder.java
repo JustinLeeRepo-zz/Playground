ea0eaafb16780e01911c6d4749785741d7ea2b7f
Justin Lee <justinj.lee23@gmail.com>
2015-03-22 19:30:39 -0700

    enqueue animalQueueLinkedList function

--- AnimalShelterQueueLinkedListOrder.java
+++ AnimalShelterQueueLinkedListOrder.java
@@ -2,7 +2,7 @@ public abstract class Animal {
 	private int order;
 	protected String name;
 	public Animal(string n){
-		none = n;
+		name = n;
 	}
 
 	public void setOrder(int n){
@@ -15,5 +15,33 @@ public abstract class Animal {
 
 	public boolean isOlderThan(Animal a){
 		return this.order > a.getOrder();
+	}
+}
+
+public class animalQueue {
+	LinkedList<Dog> dogs = new LinkedList<Dog>;
+	LinkedList<Cat> cats = new LinkedList<Cat>;
+	private int order = 0;
+	
+	public void enqueue(Animal a){
+		a.setOrder(order++);
+		if(a instanceof Dog){
+			dogs.addLast((Dog) a);
+		}
+		else if(a instanceof Cat){
+			cats.addLast((Cat) a);
+		}
+		else{
+			throw Exception("not dog or cat!")
+		}
+	}
+	
+	public Animal dequeAny(Animal a){
+		if (dog.size == 0){
+			
+		}
+		else if (cat.size == 0){
+			
+		}
 	}
 }
